d6e0c772da2d8c1b4131710be4a6a7df2b680e41
Ben McMaster <ben@benmcmaster.com>
2023-02-23 15:58:23 -0800

    beat more games

--- scripts/win2.js
+++ scripts/win2.js
@@ -1,10 +1,16 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const contractName = "Game1";
+const gameAddr = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const contractName = "Game2";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
+
+    const setXTx = await game.setX(10);
+    const receiptSetXTx = await setXTx.wait();
+
+    const setYTx = await game.setY(40);
+    const receiptSetYTx = await setYTx.wait();
 
     // do whatever you need to do to win the game here:
     const tx = await game.win();
--- scripts/win5.js
+++ scripts/win5.js
@@ -1,10 +1,16 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const contractName = "Game1";
+const gameAddr = "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0";
+const contractName = "Game5";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
+
+    const setAllowanceTx = await game.giveMeAllowance(10000);
+    const receiptSetAllowanceTx = await setAllowanceTx.wait();
+
+    const mintTx = await game.mint(10000);
+    const receiptMintTx = await mintTx.wait();
 
     // do whatever you need to do to win the game here:
     const tx = await game.win();
